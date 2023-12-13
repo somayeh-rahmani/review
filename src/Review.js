777e99d23a04d2f26176723dd43c76be398091cc
Somayeh Rahmani <rahmani.it27@gmail.com>
2023-12-13 18:38:46 +0330

    four commit

--- src/Review.js
+++ src/Review.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import people from './data';
-// import { FaChevronLeft, FaChevronRight,FaQuoteRight } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight,FaQuoteRight } from "react-icons/fa";
 
 
 
@@ -11,7 +11,22 @@ const Review = () => {
         <article className="review">
             <div className="img-container">
                 <img src={image} alt={name} className="person-img" />
+                <span className="quote-icon">
+                <FaQuoteRight />
+                </span>
             </div>
+            <h4 className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+            <div className="button-container">
+            <button className="prev-btn">
+            <FaChevronLeft />
+            </button>
+            <button className="next-btn">
+            <FaChevronRight />
+            </button>
+            </div>
+            <button className="random-btn">Suprise Me</button>
         </article>
     )
 };
